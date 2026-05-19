80171a0d5871dcb6907064e6421af52986f4d23a
ruam <roamsaleh@gmail.com>
2026-05-19 11:20:48 +0300

    package resolved

--- Scene/Models/Item.swift
+++ Scene/Models/Item.swift
@@ -4,14 +4,14 @@
 //
 //  Created by Yousra Abdelrahman on 19/11/1447 AH.
 //
-
+//
 import Foundation
 import SwiftData
 
 @Model
 final class Item {
     var timestamp: Date
-    
+    var title: String?// ruam edit
     init(timestamp: Date) {
         self.timestamp = timestamp
     }
